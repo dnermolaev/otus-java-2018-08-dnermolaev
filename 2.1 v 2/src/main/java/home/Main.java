39dff17d28e6616967c038a9c4c81e69f48820cd
Dmitrii <dermolaev@ngs.ru>
2018-09-25 10:37:45 +0700

    First commit

--- 2.1 v 2/src/main/java/home/Main.java
+++ 2.1 v 2/src/main/java/home/Main.java
@@ -1,24 +1,88 @@
 package home;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Main {
 
+    public int a;
+    public Main link;
+    public String str;
+    public String str1;
+    public int [] array;
+    public int [] array1;
+    public ArrayList <String> list;
+    public ArrayList <String> list1;
+
+
+    public Main() {
+        a = 5;
+        str = "";
+        str1="String + String + String";
+        link = null;
+        list = new ArrayList<>();
+        list1 = new ArrayList<>();
+        list1.add("Sun");
+        list1.add("Moon");
+        list1.add("Yellow");
+        array = new int [5];
+        array1 = new int [1000];
+    }
+
     public static void main(String[] args) {
-        printObjectSize(new Object());
-        //System.out.println("Object, size=" + MemoryCounter.instance().getInstrumentation().getObjectSize(new Object()));
-        //printObjectSize(new A());
+
+        System.out.println("Object, size=" + MemoryCounter.instance().getInstrumentation().getObjectSize(new Object()));
+
+        System.out.println("Empty string, size=" + MemoryCounter.instance().getInstrumentation().getObjectSize(""));
         printObjectSize(1);
-        printObjectSize("string");
         //printObjectSize(Calendar.getInstance());
         //printObjectSize(new BigDecimal("999999999999999.999"));
-        printObjectSize(new int[10]);
-        printObjectSize(new ArrayList<String>());
+        //printObjectSize(new int[10]);
+        System.out.println("int[10], size=" + MemoryCounter.instance().getInstrumentation().getObjectSize(new int[10]));
+        System.out.println("int[100], size=" + MemoryCounter.instance().getInstrumentation().getObjectSize(new int[100]));
+        System.out.println("int[1000], size=" + MemoryCounter.instance().getInstrumentation().getObjectSize(new int[1000]));
+        //printObjectSize(new ArrayList<String>());
+        System.out.println("List <String>Empty, size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(new ArrayList<String>()));
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Lena");
+        list.add("Dima");
+        list.add("cat");
+        System.out.println("List <String> 3, size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(list));
+        System.out.println();
+        //printObjectSize(example.array);
 
-        //printObjectSize(new Integer[100]);
+        Main example = null;
+        try {
+            Class clazz = Class.forName(Main.class.getName());
+            example = (Main) clazz.newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        printObjectSize(example);
+        printObjectSize(example.a);
+        System.out.println("Empty string, size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(example.str));
+        //printObjectSize(example.str);
+        System.out.println("Not empty string, size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(example.str1));
+        //printObjectSize(example.str1);
+        System.out.println("List <String>Empty, size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(example.list));
+        //printObjectSize(example.list);
+        System.out.println("List <String> 3, size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(example.list1));
+        //printObjectSize(example.list1);
+        System.out.println("int[5], size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(example.array));
+        //printObjectSize(example.array);
+        System.out.println("int[1000], size=" + MemoryCounter.instance().getInstrumentation()
+                .getObjectSize(example.array1));
+        //printObjectSize(example.array1);
     }
 
     public static void printObjectSize(Object obj) {
